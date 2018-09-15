b810146d5f544dfb9d9346c776d007197111a863
Dale Myszewski II <dale.myszewski@seagate.com>
2018-09-15 18:45:00 -0500

    Prod build fixes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/bendrocorp-app')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/bendrocorp-app/index.html'));
