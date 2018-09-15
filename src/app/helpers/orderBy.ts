951894c00fd06a8321102ba55df4ea819d8ea8d5
Dale Myszewski II <dale.myszewski@seagate.com>
2018-09-15 17:31:03 -0500

    Prod build fixes

--- src/app/helpers/orderBy.ts
+++ src/app/helpers/orderBy.ts
@@ -46,47 +46,47 @@ export class OrderByPipe
 /**
  * Helps you order things
  */
-export class OrderBy
-{
-    /**
-     * Order an object array by an attribute
-     * @param array The object array you want to order
-     * @param orderBy The
-     * @param asc 
-     */
-    static order(array, orderBy, asc = true){
+// export class OrderBy
+// {
+//     /**
+//      * Order an object array by an attribute
+//      * @param array The object array you want to order
+//      * @param orderBy The
+//      * @param asc 
+//      */
+//     static order(array, orderBy, asc = true){
 
-        if (!orderBy || orderBy.trim() == ""){
-            return array;
-        } 
+//         if (!orderBy || orderBy.trim() == ""){
+//             return array;
+//         } 
 
-        //ascending
-        if (asc){
-            return Array.from(array).sort((item1: any, item2: any) => { 
-                return this.orderByComparator(item1[orderBy], item2[orderBy]);
-            });
-        }else{
-            //not asc
-            return Array.from(array).sort((item1: any, item2: any) => { 
-                return this.orderByComparator(item2[orderBy], item1[orderBy]);
-            });
-        }
+//         //ascending
+//         if (asc){
+//             return Array.from(array).sort((item1: any, item2: any) => { 
+//                 return this.orderByComparator(item1[orderBy], item2[orderBy]);
+//             });
+//         }else{
+//             //not asc
+//             return Array.from(array).sort((item1: any, item2: any) => { 
+//                 return this.orderByComparator(item2[orderBy], item1[orderBy]);
+//             });
+//         }
 
-    }
+//     }
 
- private static orderByComparator(a:any, b:any):number{
+//  private static orderByComparator(a:any, b:any):number{
 
-     if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
-       //Isn't a number so lowercase the string to properly compare
-       if(a.toLowerCase() < b.toLowerCase()) return -1;
-       if(a.toLowerCase() > b.toLowerCase()) return 1;
-     }
-     else{
-       //Parse strings as numbers to compare properly
-       if(parseFloat(a) < parseFloat(b)) return -1;
-       if(parseFloat(a) > parseFloat(b)) return 1;
-      }
+//      if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
+//        //Isn't a number so lowercase the string to properly compare
+//        if(a.toLowerCase() < b.toLowerCase()) return -1;
+//        if(a.toLowerCase() > b.toLowerCase()) return 1;
+//      }
+//      else{
+//        //Parse strings as numbers to compare properly
+//        if(parseFloat(a) < parseFloat(b)) return -1;
+//        if(parseFloat(a) > parseFloat(b)) return 1;
+//       }
 
-     return 0; //equal each other
- }
-}
+//      return 0; //equal each other
+//  }
+// }
