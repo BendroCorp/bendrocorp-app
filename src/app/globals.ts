3c41ab0003e86222926cc34f716b2306d9be0335
Dale Myszewski II <dale.myszewski@gmail.com>
2019-07-07 16:59:37 -0500

    Improvements to the log page

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,5 +5,6 @@ import { environment } from '../environments/environment';
 @Injectable()
 export class Globals {
   baseUrlRoot: string = (environment.production) ? 'https://api.bendrocorp.com/' : 'http://localhost:3000/';
-  baseUrl: string = this.baseUrlRoot + 'api/';  
+  baseUrl: string = this.baseUrlRoot + 'api/';
+  wssUri: string = (environment.production) ? 'wss://api.bendrocorp.com/cable' : 'ws://localhost:3000/cable';
 }
--- src/app/site-logs/site-logs.component.ts
+++ src/app/site-logs/site-logs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SiteLog } from '../models/misc-models';
 import { SiteLogService } from './site-log.service';
 import { SpinnerService } from '../misc/spinner/spinner.service';
@@ -6,39 +6,69 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { MessageService } from '../message/message.service';
+import { ActionCableService, Channel } from 'angular2-actioncable';
+import { Subscription } from 'rxjs';
+import { Globals } from '../globals';
 
 @Component({
   selector: 'app-site-logs',
   templateUrl: './site-logs.component.html',
   styleUrls: ['./site-logs.component.css']
 })
-export class SiteLogsComponent implements OnInit {
+export class SiteLogsComponent implements OnInit, OnDestroy {
+  logSubscription: Subscription;
   siteLogs:SiteLog[] = []
   canViewLogs: boolean = this.authService.hasClaim(2)
+  logRefreshInProgress: boolean;
+  initialLoadComplete: boolean = false;
 
-  constructor(private siteLogService:SiteLogService, private spinnerService:SpinnerService, private authService:AuthService, private router:Router, private messageService:MessageService) { }
+  constructor(private globals: Globals,
+    private siteLogService:SiteLogService, 
+    private spinnerService:SpinnerService, 
+    private authService:AuthService, 
+    private router:Router, 
+    private messageService:MessageService,
+    private cableService: ActionCableService) { }
 
   fetchLogs()
   {
+    this.logRefreshInProgress = true;
     this.siteLogService.list().subscribe(
       (results) => {
         if (!(results instanceof HttpErrorResponse)) {
           this.siteLogs = results.sort((a, b) => {
             return b.id - a.id
-          })
-          this.spinnerService.spin(false)
+          });
+
+          this.initialLoadComplete = true;
         }
+        this.spinnerService.spin(false);
+        this.logRefreshInProgress = false;
       }
     )    
   }
 
   ngOnInit() {
     if (this.canViewLogs) {
-      this.spinnerService.spin(true)
+      const channel: Channel = this.cableService
+      .cable(`${this.globals.wssUri}/?token=${this.authService.retrieveUserSession().token}`)
+      .channel('LogChannel');
+
+      this.logSubscription = channel.received().subscribe(message => {
+          this.fetchLogs();
+      });
+
+      this.spinnerService.spin(true);
       this.fetchLogs();
     } else {
-      this.messageService.addError('You are not authorized to view this page!')
-      this.router.navigateByUrl('/')
+      this.messageService.addError('You are not authorized to view this page!');
+      this.router.navigateByUrl('/');
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.logSubscription) {
+      this.logSubscription.unsubscribe();
     }
   }
 
